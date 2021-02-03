4acc551e994c8597f6c01fd6555a5884de0b1963
Lulmao <70194529+yxchia98@users.noreply.github.com>
2021-02-04 00:55:29 +0800

    Testing

--- spooding/src/spooding/Spood.java
+++ spooding/src/spooding/Spood.java
@@ -1,7 +1,22 @@
 package spooding;
 
+import org.jsoup.Jsoup;
+import com.gargoylesoftware.htmlunit.*;
+import com.gargoylesoftware.htmlunit.html.HtmlPage;
+
 public class Spood {
-	public static void main(String []args) {
-		System.out.println("Hello World!");
+	public static void main(String[] args) {
+		String baseUrl = "https://news.ycombinator.com";
+		WebClient client = new WebClient();
+		client.getOptions().setCssEnabled(false);
+		client.getOptions().setJavaScriptEnabled(false);
+		try {
+			HtmlPage page = client.getPage(baseUrl);
+			System.out.println(page.asXml());
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
 	}
+
 }

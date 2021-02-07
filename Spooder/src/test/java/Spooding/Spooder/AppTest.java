dd5d0757e3ae801d6fa48ef131424d1bd2f759df
Lulmao <70194529+yxchia98@users.noreply.github.com>
2021-02-07 23:01:39 +0800

    test

--- Spooder/src/test/java/Spooding/Spooder/AppTest.java
+++ Spooder/src/test/java/Spooding/Spooder/AppTest.java
@@ -2,7 +2,15 @@ package Spooding.Spooder;
 
 import static org.junit.Assert.assertTrue;
 
+import java.io.IOException;
+import java.util.List;
+
 import org.junit.Test;
+
+import com.gargoylesoftware.htmlunit.WebClient;
+import com.gargoylesoftware.htmlunit.html.HtmlAnchor;
+import com.gargoylesoftware.htmlunit.html.HtmlElement;
+import com.gargoylesoftware.htmlunit.html.HtmlPage;
 
 /**
  * Unit test for simple App.
@@ -11,10 +19,22 @@ public class AppTest
 {
     /**
      * Rigorous Test :-)
+     * @throws IOException 
      */
     @Test
-    public void shouldAnswerWithTrue()
+    public void shouldAnswerWithTrue() throws IOException
     {
+    	String url = "https://www.reddit.com/search/?q=gamestop";
+        System.out.println("Fetching " + url + "...");
+        WebClient client = new WebClient();
+        client.getOptions().setCssEnabled(false);
+        client.getOptions().setJavaScriptEnabled(false);
+        HtmlPage page = client.getPage(url);
+        List<HtmlElement> itemList = page.getByXPath("//a[@data-click-id='body']");
+        for (HtmlElement item : itemList) {
+//        	System.out.println(item);
+        	System.out.println(item.getTextContent() + ": " + ((HtmlAnchor) item).getAttribute("href"));
+        }
         assertTrue( true );
     }
 }

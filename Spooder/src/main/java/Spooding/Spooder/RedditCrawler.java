9a0f01cdfc2233a68cfee91e8eef193e658c0559
Lulmao <70194529+yxchia98@users.noreply.github.com>
2021-02-23 16:43:51 +0800

    votes

--- Spooder/src/main/java/Spooding/Spooder/RedditCrawler.java
+++ Spooder/src/main/java/Spooding/Spooder/RedditCrawler.java
@@ -1,8 +1,9 @@
 package Spooding.Spooder;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.TimeUnit;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.*;
 
@@ -19,12 +20,10 @@ public class RedditCrawler extends Crawler {
 	public void setBaseUrl(String baseUrl) {
 		this.baseUrl = baseUrl;
 	}
-	
+
 	public RedditCrawler(String baseUrl) {
 		this.baseUrl = baseUrl;
 	}
-
-
 
 	@Override
 	public void crawl() throws InterruptedException {
@@ -37,9 +36,9 @@ public class RedditCrawler extends Crawler {
 		ChromeOptions options = new ChromeOptions();
 		options.addArguments("--headless", "--disable-gpu", "--window-size=1920,1200", "--ignore-certificate-errors",
 				"--disable-extensions", "--no-sandbox", "--disable-dev-shm-usage");
+		System.setProperty(ChromeDriverService.CHROME_DRIVER_SILENT_OUTPUT_PROPERTY, "true");
+		Logger.getLogger("org.openqa.selenium").setLevel(Level.OFF);
 		WebDriver driver = new ChromeDriver(options);
-//				WebDriverWait wait = new WebDriverWait(driver, 40);
-//		driver.manage().window().maximize();
 
 //Deleting all the cookies
 		driver.manage().deleteAllCookies();
@@ -52,29 +51,33 @@ public class RedditCrawler extends Crawler {
 		driver.get(getBaseUrl());
 		JavascriptExecutor js = (JavascriptExecutor) driver;
 		js.executeScript("window.scrollBy(0,document.body.scrollHeight)");
-		Thread.sleep(3500); //pause the browser to let javascript load new posts
-		List<WebElement> list = driver.findElements(By.xpath("//a[@data-click-id='body']"));
-		
+		Thread.sleep(4000); // pause the browser to let javascript load new posts
+		List<WebElement> list = driver.findElements(By.xpath("//div[contains(@class, '_1oQyIsiPHYt6nx7VOmd1sz')]"));
+
 		for (WebElement listItem : list) {
 			count++;
-			System.out.println(count + ": " + listItem.getText()); //post title
-			System.out.println(listItem.getAttribute("href"));
-			js.executeScript("arguments[0].click();", listItem);
-			List<WebElement> postTextList = driver.findElements(By.xpath("//div[@data-click-id='text']"));
-			postText = "";
-			for (WebElement postTextContent : postTextList) {
-				postText += postTextContent.getText() + "\n";
-			}
-			System.out.println(postText); //post content
-			js.executeScript("window.history.back();");
-
+			WebElement postTitle = listItem.findElement(By.xpath(".//a[@data-click-id='body']"));
+			WebElement postVote = listItem.findElement(By.xpath(".//div[@class='_1E9mcoVn4MYnuBQSVDt1gC']"));
+			System.out.println(count + ": " + postTitle.getText()); //Post Title
+			System.out.println("Votes: " + postVote.getText()); // Post votes
+			System.out.println("Link: " + postTitle.getAttribute("href")); //Post Link
+			//goes into the post and extracts post contents, nullify this code if you want the crawler to run significantly faster.
+//			js.executeScript("arguments[0].click();", listItem);
+//			List<WebElement> postTextList = driver.findElements(By.xpath("//div[@data-click-id='text']"));
+//			postText = "";
+//			for (WebElement postTextContent : postTextList) {
+//				postText += postTextContent.getText() + "\n";
+//			}
+//			System.out.println(postText); //post content
+//			js.executeScript("window.history.back();");
+			//end of post content extraction
 		}
 		driver.close();
 		driver.quit();
 	}
-	
+
 	public void export() {
-		
+
 	}
 
 }

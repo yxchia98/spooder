3294e15f6f770fee1d4334a889f5c156171a212f
Lulmao <70194529+yxchia98@users.noreply.github.com>
2021-02-23 12:08:42 +0800

    Update App.java

--- Spooder/src/main/java/Spooding/Spooder/App.java
+++ Spooder/src/main/java/Spooding/Spooder/App.java
@@ -11,7 +11,7 @@ public class App {
 	public static void main(String[] args) throws IOException, InterruptedException, TwitterException {
 		Boolean proceed = true;
 		String url;
-		int choice;
+		int choice, subChoice;
 		Scanner input = new Scanner(System.in);
 		System.out.print("Enter search string:");
 		String searchString = input.next();
@@ -24,15 +24,30 @@ public class App {
 
 		while (proceed) {
 			System.out.print(
-					"----------MAIN MENU----------\n1. Crawl from twitter\n2. Crawl from reddit\n3. Sentimental analysis with current dataset\n4. Exit\nEnter choice:");
+					"----------MAIN MENU----------\n1. Crawl from all sources\n2. Crawl from specific source\n3. Sentimental analysis with current dataset\n4. Exit\nEnter choice: ");
 			choice = input.nextInt();
 			switch (choice) {
 			case 1:
+				redditCrawl.crawl();
 				TwitterCrawler.twitterStart();
 				twitterCrawler.crawl();
 				break;
 			case 2:
-				redditCrawl.crawl();
+				System.out.print("----------Specific crawl----------\n1. Crawl from twitter\n2. Crawl from reddit\n3. return\nChoice: ");
+				subChoice = input.nextInt();
+				switch(subChoice) {
+				case 1:
+					TwitterCrawler.twitterStart();
+					twitterCrawler.crawl();
+					break;
+				case 2:
+					redditCrawl.crawl();
+					break;
+				case 3:
+					break;
+				default: 
+					break;
+				}
 				break;
 			case 3:
 				break;

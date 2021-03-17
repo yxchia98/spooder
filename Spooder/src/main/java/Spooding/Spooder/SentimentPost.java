a81c78468857cfd20998c76d101ddf0e045815ea
kenzothewise <kenzohari@hotmail.com>
2021-03-18 01:30:48 +0800

    Added comments to SentimentPost class
    Added comments and description for variables and functions to SentimentPost class. To be generated in Java documentation.

--- Spooder/src/main/java/Spooding/Spooder/SentimentPost.java
+++ Spooder/src/main/java/Spooding/Spooder/SentimentPost.java
@@ -1,41 +1,78 @@
 package Spooding.Spooder;
 
-
+/**
+ * Sentiment post class used to store data to be exported to MongoDB database
+ * Creates a mongoDB collection for string sentiment 
+ *
+ */
 public class SentimentPost {
-	private String title;
-	private String sentiment;
-	private String source;
+	private String title; // Variable for post title
+	private String sentiment; // Variable for posts' sentiment
+	private String source; // Variable for post source (Reddit, Twitter, Straitstimes)
 
+	/**
+	 * Constructor
+	 */
 	public SentimentPost() {
 
 	}
 
+	/**
+	 * Constructor to create SentimentPost object containing post title, sentiment and source
+	 * @param title
+	 * @param sentiment
+	 * @param source
+	 */
 	public SentimentPost(String title, String sentiment, String source) {
 		this.title = title;
 		this.sentiment = sentiment;
 		this.source = source;
 	}
 
+	/**
+	 * Get Method to return title string
+	 * @return title
+	 */
 	public String getTitle() {
 		return title;
 	}
 
+	/**
+	 * Set Method to modify title string
+	 * @param title
+	 */
 	public void setTitle(String title) {
 		this.title = title;
 	}
 
+	/**
+	 * Get Method to return sentiment string
+	 * @return sentiment
+	 */
 	public String getSentiment() {
 		return sentiment;
 	}
 
+	/**
+	 * Set Method to modify sentiment string
+	 * @param sentiment
+	 */
 	public void setSentiment(String sentiment) {
 		this.sentiment = sentiment;
 	}
 
+	/**
+	 * Get Method to return source string
+	 * @return source
+	 */
 	public String getSource() {
 		return source;
 	}
 
+	/**
+	 * Set Method to modify Source string
+	 * @param source
+	 */
 	public void setSource(String source) {
 		this.source = source;
 	}
